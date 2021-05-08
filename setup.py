8ff6d511be1629b99413019d39ce54b430736704
Djordje Atlialp <djordje@atlialp.com>
2021-05-08 14:50:12 +0200

    Add versions to doc

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Environment :: Web Environment",
         "Topic :: Software Development",
         "Topic :: Software Development :: User Interfaces",
